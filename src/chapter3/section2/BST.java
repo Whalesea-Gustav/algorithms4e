1ef5e4e4be66ae0f26bfed8a2bb4aa6d310db668
whalesea <asipirinofjim@gmail.com>
2019-01-29 01:41:46 +0800

    完善BST

--- src/chapter3/section2/BST.java
+++ src/chapter3/section2/BST.java
@@ -1,5 +1,6 @@
 package chapter3.section2;
-
+import org.junit.Test;
+import org.junit.Assert;
 public class BST <Key extends Comparable<Key>, Value> {
     private Node root;
     private class Node {
@@ -14,7 +15,22 @@ public class BST <Key extends Comparable<Key>, Value> {
             this.N = n;
         }
     }
-
+    @Test
+    public void testForInitiation() {
+        BST<Character, Integer> tree = new BST<>();
+        tree.put('A', 10);
+        tree.put('B', 20);
+        tree.put('D', 30);
+        tree.put('C', 40);
+        Assert.assertEquals(tree.size(), 4);
+        Assert.assertEquals(tree.get('A'), (Integer) 10);
+        Assert.assertEquals(tree.min(), (Character) 'A');
+        Assert.assertEquals(tree.max(), (Character) 'D');
+        Assert.assertEquals(tree.floor('E'), (Character) 'D');
+        Assert.assertEquals(tree.floor('B'), (Character) 'B');
+        Assert.assertEquals(tree.rank('B'), 1);
+        Assert.assertEquals(tree.rank('C'), 2);
+    }
 
     public int size() { return size(root); }
 
@@ -26,7 +42,6 @@ public class BST <Key extends Comparable<Key>, Value> {
     public Value get(Key key) {
         return get(root, key);
     }
-
     private Value get(Node rt, Key k) {
             if (rt == null) return null;
 
@@ -39,7 +54,6 @@ public class BST <Key extends Comparable<Key>, Value> {
     public void put(Key key, Value val) {
         root = put(root, key, val);
     }
-
     public Node put(Node rt, Key k, Value v) {
         //如果k存在于root中，则更新它的值
         //否则new一个Node，并插入到树的节点中
@@ -86,6 +100,21 @@ public class BST <Key extends Comparable<Key>, Value> {
         }
     }
 
+    public Key ceiling(Key k) {
+        return ceiling(root, k).key;
+    }
+    // find the Node with maximal key of >= k 找到 >= 给定Key的最小Node
+    private Node ceiling(Node rt, Key k) {
+        if (rt == null) return null;
+        int cmp = rt.key.compareTo(k);
+        if (cmp < 0) return ceiling(rt.right, k);
+        else {
+            Node temp = ceiling(rt.left , k);
+            if (temp == null) return rt;
+            else return temp;
+        }
+    }
+
     public Key select(int k) {
         return select(root, k).key;
     }
@@ -106,7 +135,65 @@ public class BST <Key extends Comparable<Key>, Value> {
         if (cmp < 0) return size(rt.left) + 1 + rank(rt.right, k);
         else if (cmp > 0) return rank(rt.left, k);
         else return size(rt.left);
-        
     }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+    private Node deleteMin(Node rt) {
+        if (rt.left == null) return rt.right;
+        rt.left = deleteMin(rt.left);
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+    }
+
+    public void deleteMax() { root = deleteMax(root); }
+    private Node deleteMax(Node rt) {
+        if (rt.right == null) return rt.left;
+        rt.right = deleteMax(rt.right);
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+    }
+
+    public void delete(Key key) {
+        root = deletePredecessor(root, key);
+    }
+    //使用先驱节点
+    private Node deletePredecessor(Node rt, Key k) {
+        if (rt == null) return null;
+        int cmp = k.compareTo(rt.key);
+        if (cmp > 0) rt.right = deletePredecessor(rt.right, k);
+        else if (cmp < 0) rt.left =  deletePredecessor(rt.left, k);
+        else { // rt.key.equals(k)
+            if (rt.right == null) return rt.left;
+            if (rt.left == null) return rt.right;
+            Node temp = rt;
+            rt = max(temp.left);
+            rt.left = deleteMax(temp.left);
+            rt.right = temp.right;
+        }
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+    }
+    //使用后继节点
+    private Node deleteSuccessor(Node rt, Key k) {
+        if (rt == null) return null;
+        int cmp =  k.compareTo(rt.key);
+        if (cmp > 0) rt.right = deleteSuccessor(rt.right, k);
+        else if (cmp < 0) rt.left = deleteSuccessor(rt.left, k);
+        else {
+            if (rt.right == null) return rt.left;
+            if (rt.left == null) return rt.right;
+            Node temp = rt;
+            rt = min(rt.right);
+            rt.right = deleteMin(temp.right);
+            rt.left = temp.left;
+        }
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+
+    }
+
+    
 
 }
---
+++ src/test/chapter2/section3/P22Test.java
@@ -0,0 +1,55 @@
+package test.chapter2.section3; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* P22 Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jan 29, 2019</pre> 
+* @version 1.0 
+*/ 
+public class P22Test { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: exch(Comparable[] arr, int index1, int index2) 
+* 
+*/ 
+@Test
+public void testExch() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: quick3way(Comparable[] arr, int lo, int hi) 
+* 
+*/ 
+@Test
+public void testQuick3way() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: testForQuick3way() 
+* 
+*/ 
+@Test
+public void testTestForQuick3way() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
