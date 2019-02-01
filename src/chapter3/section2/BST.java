9866388f18921f07780c71c02ac09b07522dc51e
Whalesea-Gustav <asipirinofjim@gmail.com>
2019-02-01 22:20:49 +0800

    maybe some changes

--- src/chapter3/section2/BST.java
+++ src/chapter3/section2/BST.java
@@ -1,5 +1,7 @@
 package chapter3.section2;
-
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.LinkedList;
 public class BST <Key extends Comparable<Key>, Value> {
     private Node root;
     private class Node {
@@ -14,6 +16,25 @@ public class BST <Key extends Comparable<Key>, Value> {
             this.N = n;
         }
     }
+<<<<<<< HEAD
+=======
+    @Test
+    public void testForInitiation() {
+        BST<Character, Integer> tree = new BST<>();
+        tree.put('A', 10);
+        tree.put('B', 20);
+        tree.put('D', 30);
+        tree.put('C', 40);
+        Assert.assertEquals(tree.size(), 4);
+        Assert.assertEquals(tree.get('A'), (Integer) 10);
+        Assert.assertEquals(tree.min(), (Character) 'A');
+        Assert.assertEquals(tree.max(), (Character) 'D');
+        Assert.assertEquals(tree.floor('E'), (Character) 'D');
+        Assert.assertEquals(tree.floor('B'), (Character) 'B');
+        Assert.assertEquals(tree.rank('B'), 1);
+        Assert.assertEquals(tree.rank('C'), 2);
+    }
+>>>>>>> a317f1f52a7053542a52542f60e846b440def5f9
 
     public int size() { return size(root); }
     private int size(Node root) {
@@ -85,12 +106,20 @@ public class BST <Key extends Comparable<Key>, Value> {
     public Key ceiling(Key k) {
         return ceiling(root, k).key;
     }
+<<<<<<< HEAD
+=======
+    // find the Node with maximal key of >= k 找到 >= 给定Key的最小Node
+>>>>>>> a317f1f52a7053542a52542f60e846b440def5f9
     private Node ceiling(Node rt, Key k) {
         if (rt == null) return null;
         int cmp = rt.key.compareTo(k);
         if (cmp < 0) return ceiling(rt.right, k);
         else {
+<<<<<<< HEAD
             Node temp = ceiling(rt.left, k);
+=======
+            Node temp = ceiling(rt.left , k);
+>>>>>>> a317f1f52a7053542a52542f60e846b440def5f9
             if (temp == null) return rt;
             else return temp;
         }
@@ -116,6 +145,7 @@ public class BST <Key extends Comparable<Key>, Value> {
         if (cmp < 0) return size(rt.left) + 1 + rank(rt.right, k);
         else if (cmp > 0) return rank(rt.left, k);
         else return size(rt.left);
+<<<<<<< HEAD
 
     }
 
@@ -162,6 +192,41 @@ public class BST <Key extends Comparable<Key>, Value> {
         if (cmp > 0) rt.left = deleteWithPredecessor(rt.left, k);
         else if (cmp < 0) rt.right = deleteWithPredecessor(rt.right, k);
         else { // rt.key == k, 使用前趋节点 -- 左树的最大值
+=======
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+    private Node deleteMin(Node rt) {
+        if (rt.left == null) return rt.right;
+        rt.left = deleteMin(rt.left);
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+    }
+
+    public void deleteMax() { root = deleteMax(root); }
+    private Node deleteMax(Node rt) {
+        if (rt.right == null) return rt.left;
+        rt.right = deleteMax(rt.right);
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+    }
+
+    public void delete(Key key) {
+        root = deletePredecessor(root, key);
+        //root = deleteSuccessor(root, key);
+    }
+    //使用先驱节点
+    private Node deletePredecessor(Node rt, Key k) {
+        if (rt == null) return null;
+        int cmp = k.compareTo(rt.key);
+        if (cmp > 0) rt.right = deletePredecessor(rt.right, k);
+        else if (cmp < 0) rt.left =  deletePredecessor(rt.left, k);
+        else { // rt.key.equals(k)
+            if (rt.right == null) return rt.left;
+            if (rt.left == null) return rt.right;
+>>>>>>> a317f1f52a7053542a52542f60e846b440def5f9
             Node temp = rt;
             rt = max(temp.left);
             rt.left = deleteMax(temp.left);
@@ -170,6 +235,47 @@ public class BST <Key extends Comparable<Key>, Value> {
         rt.N = size(rt.left) + size(rt.right) + 1;
         return rt;
     }
+<<<<<<< HEAD
+=======
+    //使用后继节点
+    private Node deleteSuccessor(Node rt, Key k) {
+        if (rt == null) return null;
+        int cmp =  k.compareTo(rt.key);
+        if (cmp > 0) rt.right = deleteSuccessor(rt.right, k);
+        else if (cmp < 0) rt.left = deleteSuccessor(rt.left, k);
+        else {
+            if (rt.right == null) return rt.left;
+            if (rt.left == null) return rt.right;
+            Node temp = rt;
+            rt = min(rt.right);
+            rt.right = deleteMin(temp.right);
+            rt.left = temp.left;
+        }
+        rt.N = size(rt.left) + size(rt.right) + 1;
+        return rt;
+
+    }
+
+    public Iterable<Key> keys() {
+        return keys(this.min(), this.max());
+    }
+    private Iterable<Key> keys(Key lo, Key hi) {
+        LinkedList<Key> answer = new LinkedList<>();
+        keys(root, answer, lo, hi);
+        return answer;
+    }
+    private void keys(Node rt, LinkedList<Key> answer, Key lo, Key hi) {
+        if (rt == null) return;
+        int cmplo = lo.compareTo(rt.key);
+        int cmphi = hi.compareTo(rt.key);
+        if (cmplo <= 0 && cmphi >= 0) answer.add(rt.key);
+        if (cmplo < 0) keys(rt.left, answer, lo, hi);
+        if (cmphi > 0) keys(rt.right, answer, lo, hi);
+    }
+
+
+
+>>>>>>> a317f1f52a7053542a52542f60e846b440def5f9
 }
 
 
